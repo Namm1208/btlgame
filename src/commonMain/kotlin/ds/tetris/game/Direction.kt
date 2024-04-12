6cef2035880d374bbafcc3fc2de9283e5e544a01
Namm1208 <125287992+Namm1208@users.noreply.github.com>
2024-04-13 01:46:21 +0700

    Thêm tệp dự án Tetris

---
+++ src/commonMain/kotlin/ds/tetris/game/Direction.kt
@@ -0,0 +1,13 @@
+
+
+package ds.tetris.game
+
+import androidx.compose.ui.unit.IntOffset
+
+enum class Direction(val movement: IntOffset) {
+    UP(IntOffset(0, -1)),
+    DOWN(IntOffset(0, 1)),
+    LEFT(IntOffset(-1, 0)),
+    RIGHT(IntOffset(1, 0))
+}
+
